6c65fcdfbc7b1df35c304363ca23f4fe1acc5b5c
jacksonmonteiro <infor.jackson324@gmail.com>
2022-04-20 11:46:07 -0300

    Admin Model and Admin Repository

--- lib/src/repositories/admin_repository.dart
+++ lib/src/repositories/admin_repository.dart
@@ -1,6 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:emplox/src/models/admin_model.dart';
 
 class AdminRepository {
   final Dio dio = Dio();
   final String url = 'http://localhost:3000/';
+
+  Future<List<AdminModel>> fetchAdmins() async {
+    final response = await dio.get(url);
+    final list = response.data as List;
+    return list.map((json) => AdminModel.fromJson(json)).toList();
+  }
 }
